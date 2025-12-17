95b54d035de26f727eb29ebd02a3e2266ae26bb6
oihoihoih <ochyuohotmail.com@MacBook-Pro-de-Oihane-2.local>
2025-12-17 18:04:24 +0100

    Basic carousel added in js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,3 +11,25 @@
  */
 
 console.log ('Hola! Este es el archivo main.js');
+
+
+const buttonPrev = document.getElementById('slide-arrow-prev');
+const buttonNext = document.getElementById('slide-arrow-next');
+const carouselWrapper = document.getElementById('carousel-wrapper');
+const images = Array.from(carouselWrapper.querySelectorAll('img'));
+
+
+buttonNext.addEventListener('click', () => {
+    const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
+    images[visibleIndex].classList.add('hidden');
+    let newIndex = (visibleIndex + 1) % images.length;
+    images[newIndex].classList.remove('hidden');
+});
+
+buttonPrev.addEventListener('click', () => {
+          const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
+    images[visibleIndex].classList.add('hidden');
+    let newIndex = (visibleIndex - 1 + images.length) % images.length;
+    images[newIndex].classList.remove('hidden');  
+
+});
