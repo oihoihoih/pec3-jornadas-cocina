915c7fdd8c58d636903a6ec361b4248c4e1f8837
oihoihoih <ochyuo@hotmail.com>
2025-12-18 19:02:28 +0100

    Translate-x transitions added in carousel slider

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -21,15 +21,48 @@ const images = Array.from(carouselWrapper.querySelectorAll('img'));
 
 buttonNext.addEventListener('click', () => {
     const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
-    images[visibleIndex].classList.add('hidden');
-    let newIndex = (visibleIndex + 1) % images.length;
+    const newIndex = (visibleIndex + 1) % images.length;
+
+    // Current image goes out (to the left)
+    images[visibleIndex].classList.add('-translate-x-full','transition','duration-500', 'ease-in-out');
+
+    // Next image goes out of the screen
+    images[newIndex].classList.add('translate-x-full');
     images[newIndex].classList.remove('hidden');
-});
+
+    void images[newIndex].offsetWidth;
+
+    images[newIndex].classList.remove('translate-x-full');
+    images[newIndex].classList.add('translate-x-0','transition', 'duration-500', 'ease-in-out');
+
+    // Clean up classes after animation
+    setTimeout(() => {
+        images[visibleIndex].classList.add('hidden');
+        images[visibleIndex].classList.remove('-translate-x-full','transition','duration-500', 'ease-in-out');
+        images[newIndex].classList.remove('translate-x-0','transition', 'duration-500', 'ease-in-out');
+    }, 500);    
+  });
 
 buttonPrev.addEventListener('click', () => {
-          const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
-    images[visibleIndex].classList.add('hidden');
-    let newIndex = (visibleIndex - 1 + images.length) % images.length;
-    images[newIndex].classList.remove('hidden');  
+    const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
+    const newIndex = (visibleIndex + 1) % images.length;
 
+    // Current image goes out (to the left)
+    images[visibleIndex].classList.add('translate-x-full','transition','duration-500', 'ease-in-out');
+
+    // Next image goes out of the screen
+    images[newIndex].classList.add('-translate-x-full');
+    images[newIndex].classList.remove('hidden');
+
+    void images[newIndex].offsetWidth;
+
+    images[newIndex].classList.remove('-translate-x-full');
+    images[newIndex].classList.add('translate-x-0','transition', 'duration-500', 'ease-in-out');
+
+    // Clean up classes after animation
+    setTimeout(() => {
+        images[visibleIndex].classList.add('hidden');
+        images[visibleIndex].classList.remove('translate-x-full','transition','duration-500', 'ease-in-out');
+        images[newIndex].classList.remove('translate-x-0','transition', 'duration-500', 'ease-in-out');
+    }, 500);    
 });
