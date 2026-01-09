608af6e5beb7d9fd1f9efb4843de8833956df0ea
oihoihoih <ochyuo@hotmail.com>
2026-01-09 11:15:20 +0100

    Some entrance animations added with gsap

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,94 +3,153 @@
  * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+// GSAP Animation Library
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+gsap.registerPlugin(SplitText);
 
 /**
  * Write any other JavaScript below
  */
 
 // Carousel functionality
-const buttonPrev = document.getElementById('slide-arrow-prev');
-const buttonNext = document.getElementById('slide-arrow-next');
-const carouselWrapper = document.getElementById('carousel-wrapper');
+const buttonPrev = document.getElementById("slide-arrow-prev");
+const buttonNext = document.getElementById("slide-arrow-next");
+const carouselWrapper = document.getElementById("carousel-wrapper");
 
 if (carouselWrapper && buttonPrev && buttonNext) {
+  const images = Array.from(carouselWrapper.querySelectorAll("img"));
 
-const images = Array.from(carouselWrapper.querySelectorAll('img'));
-
-
-buttonNext.addEventListener('click', () => {
-    const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
+  buttonNext.addEventListener("click", () => {
+    const visibleIndex = images.findIndex(
+      (img) => !img.classList.contains("hidden")
+    );
     const newIndex = (visibleIndex + 1) % images.length;
 
     // Current image goes out (to the left)
-    images[visibleIndex].classList.add('-translate-x-full','transition','duration-500', 'ease-in-out');
+    images[visibleIndex].classList.add(
+      "-translate-x-full",
+      "transition",
+      "duration-500",
+      "ease-in-out"
+    );
 
     // Next image goes out of the screen
-    images[newIndex].classList.add('translate-x-full');
-    images[newIndex].classList.remove('hidden');
+    images[newIndex].classList.add("translate-x-full");
+    images[newIndex].classList.remove("hidden");
 
     void images[newIndex].offsetWidth;
 
-    images[newIndex].classList.remove('translate-x-full');
-    images[newIndex].classList.add('translate-x-0','transition', 'duration-500', 'ease-in-out');
+    images[newIndex].classList.remove("translate-x-full");
+    images[newIndex].classList.add(
+      "translate-x-0",
+      "transition",
+      "duration-500",
+      "ease-in-out"
+    );
 
     // Clean up classes after animation
     setTimeout(() => {
-        images[visibleIndex].classList.add('hidden');
-        images[visibleIndex].classList.remove('-translate-x-full','transition','duration-500', 'ease-in-out');
-        images[newIndex].classList.remove('translate-x-0','transition', 'duration-500', 'ease-in-out');
-    }, 500);    
+      images[visibleIndex].classList.add("hidden");
+      images[visibleIndex].classList.remove(
+        "-translate-x-full",
+        "transition",
+        "duration-500",
+        "ease-in-out"
+      );
+      images[newIndex].classList.remove(
+        "translate-x-0",
+        "transition",
+        "duration-500",
+        "ease-in-out"
+      );
+    }, 500);
   });
 
-buttonPrev.addEventListener('click', () => {
-    const visibleIndex = images.findIndex(img => !img.classList.contains('hidden'));
+  buttonPrev.addEventListener("click", () => {
+    const visibleIndex = images.findIndex(
+      (img) => !img.classList.contains("hidden")
+    );
     const newIndex = (visibleIndex + 1) % images.length;
 
     // Current image goes out (to the left)
-    images[visibleIndex].classList.add('translate-x-full','transition','duration-500', 'ease-in-out');
+    images[visibleIndex].classList.add(
+      "translate-x-full",
+      "transition",
+      "duration-500",
+      "ease-in-out"
+    );
 
     // Next image goes out of the screen
-    images[newIndex].classList.add('-translate-x-full');
-    images[newIndex].classList.remove('hidden');
+    images[newIndex].classList.add("-translate-x-full");
+    images[newIndex].classList.remove("hidden");
 
     void images[newIndex].offsetWidth;
 
-    images[newIndex].classList.remove('-translate-x-full');
-    images[newIndex].classList.add('translate-x-0','transition', 'duration-500', 'ease-in-out');
+    images[newIndex].classList.remove("-translate-x-full");
+    images[newIndex].classList.add(
+      "translate-x-0",
+      "transition",
+      "duration-500",
+      "ease-in-out"
+    );
 
     // Clean up classes after animation
     setTimeout(() => {
-        images[visibleIndex].classList.add('hidden');
-        images[visibleIndex].classList.remove('translate-x-full','transition','duration-500', 'ease-in-out');
-        images[newIndex].classList.remove('translate-x-0','transition', 'duration-500', 'ease-in-out');
-    }, 500);    
-});
+      images[visibleIndex].classList.add("hidden");
+      images[visibleIndex].classList.remove(
+        "translate-x-full",
+        "transition",
+        "duration-500",
+        "ease-in-out"
+      );
+      images[newIndex].classList.remove(
+        "translate-x-0",
+        "transition",
+        "duration-500",
+        "ease-in-out"
+      );
+    }, 500);
+  });
 }
 
 // Modal functionality
+if (window.location.pathname.includes("speakers")) {
+  const closeButton = document.getElementById("closeModalButton");
+  const openButton = document.getElementById("openModalButton");
+  const modalContainer = document.getElementById("nlModalContainer");
+  const modal = document.getElementById("nlModal");
+  const cancelModalButton = document.getElementById("cancelModalButton");
+  const acceptModalButton = document.getElementById("acceptModalButton");
 
-if (window.location.pathname.includes('speakers')){
-const closeButton = document.getElementById('closeModalButton');
-const openButton = document.getElementById('openModalButton');
-const modalContainer = document.getElementById('nlModalContainer');
-const modal = document.getElementById('nlModal');
-const cancelModalButton = document.getElementById('cancelModalButton');
-const acceptModalButton = document.getElementById('acceptModalButton');
+  closeButton.addEventListener("click", () => {
+    modalContainer.classList.add("hidden");
+  });
 
-closeButton.addEventListener('click', () => {
-    modalContainer.classList.add('hidden');
+  cancelModalButton.addEventListener("click", () => {
+    modalContainer.classList.add("hidden");
+  });
+
+  acceptModalButton.addEventListener("click", () => {
+    modalContainer.classList.add("hidden");
+  });
+
+  openButton.addEventListener("click", () => {
+    modalContainer.classList.remove("hidden");
+  });
+
+// GSAP Cards Animation
+gsap.from(".card", {
+  y: 100,
+  autoAlpha: 0,
+  stagger: 0.1,
+});
+}
+
+// Animations for blog page
+gsap.from(".blog-post, speakers", {
+  autoAlpha: 0,
+  duration: 1,
 });
 
-cancelModalButton.addEventListener('click', () => {
-    modalContainer.classList.add('hidden');
-});
 
-acceptModalButton.addEventListener('click', () => {
-    modalContainer.classList.add('hidden');
-});
-
-openButton.addEventListener('click', () => {
-    modalContainer.classList.remove('hidden');
-}); }
